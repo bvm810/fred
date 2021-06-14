fixed errorwith normalization

fred/align.py
@@ -206,7 +206,7 @@ def handle_params(form, fs):
 		"win_length": int(round((float(form["winsize"])/1000)*fs)),
 		"hop_length": int(round((1 - (float(form["overlap"])/100))*round((float(form["winsize"])/1000)*fs))),
 		"center": True, # user does not get a choice on this parameter
-		"norm": np.inf if form["norm"] == "inf" else float(form["norm"]),
+		"norm": np.inf if form["norm"] == "inf" else int(form["norm"]),
 		"epsilon": float(form["epsilon"]),
 		"gamma": float(form["gamma"]),
 		"metric": form["distance"],
@@ -271,7 +271,7 @@ def write_info_json(filepaths, form):
 		"window": params["window"],
 		"hop_length": params["hop_length"],
 		"win_length": params["win_length"],
-		"norm": params["norm"],
+		"norm": "inf" if np.isinf(params["norm"]) else params["norm"],
 		"epsilon": params["epsilon"],
 		"gamma": params["gamma"]
 	}

fred/processing.py
@@ -57,7 +57,7 @@ def chromagram(audio, fs, params):
 	chromagram = chroma_stft(
 		audio,
 		fs,
-		norm = params["norm"],
+		norm = None,
 		n_fft = params["n_fft"],
 		hop_length = params["hop_length"],
 		win_length = params["win_length"],
@@ -65,13 +65,15 @@ def chromagram(audio, fs, params):
 		center = params["center"]
 	)
 
-	# Normalization
-	for column in chromagram.T:
-		if np.linalg.norm(column, 2) < params["epsilon"]:
-			column[...] = (1/np.sqrt(12)) * np.ones(12)
-
 	# Compression
 	chromagram = np.log10(1+params["gamma"]*chromagram)
+
+	# Normalization
+	for column in chromagram.T:
+		if np.linalg.norm(column, params["norm"]) < params["epsilon"]:
+			column[...] = (1/np.linalg.norm(np.ones(12), params["norm"])) * np.ones(12)
+		else:
+			column = column/np.linalg.norm(column, params["norm"])
 
 	return chromagram
 